63020514393df61c3cfbbf67b5b26c3ac57c8c8c
digitalscorpyun <mikerkibbe73@gmail.com>
2026-01-17 16:58:47 -0800

    gf.update_screen(ai_settings, screen, ship)

--- crash_course_projects/alien_invasion/alien_invasion.py
+++ crash_course_projects/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game and create a screen object.
@@ -21,6 +22,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
