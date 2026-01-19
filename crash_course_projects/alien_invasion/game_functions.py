3e80f405f5edfe774fc766e9b8c7620d69ddf907
digitalscorpyun <mikerkibbe73@gmail.com>
2026-01-19 09:40:17 -0800

    firing bullets

--- crash_course_projects/alien_invasion/game_functions.py
+++ crash_course_projects/alien_invasion/game_functions.py
@@ -1,28 +1,14 @@
-import sys
+# game_functions.py
 import pygame
 
-def check_keydown_events(event, ship):
-    if event.key == pygame.K_RIGHT:
-        ship.moving_right = True
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = True
-
-def check_keyup_events(event, ship):
-    if event.key == pygame.K_RIGHT:
-        ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = False
-
-def check_events(ship):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
-
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
+    """Update images on the screen and flip to the new screen."""
     screen.fill(ai_settings.bg_color)
+    
+    # Rationale: Redraw all bullets behind ship and aliens.
+    # We must call the custom draw_bullet() method for each sprite.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+        
     ship.blitme()
     pygame.display.flip()
--- crash_course_projects/alien_invasion/settings.py
+++ crash_course_projects/alien_invasion/settings.py
@@ -1,18 +1,13 @@
+# settings.py
 class Settings():
-    """A class to store all settings for Alien Invasion."""
-
     def __init__(self):
-        """Initialize the game's settings."""
-        # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        # ... Screen/Ship settings ...
         self.bg_color = (255, 255, 255)
-        
-        # Ship settings
         self.ship_speed_factor = 1.5
 
         # Bullet Settings
+        # Rationale: Using underscores to create flat attributes.
         self.bullet_speed_factor = 1
-        self.bullet.width = 3
-        self.bullet.height = 15
-        self.bullet.color = (60, 60, 60)
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
